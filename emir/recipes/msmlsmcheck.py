c70a69f7e2d506ed8d4a1009d3f5a3e967377f4c
Sergio Pascual <sergiopr@fis.ucm.es>
2012-02-08 18:56:24 +0100

    MSM & LSM check Recipe

--- emir/recipes/msmlsmcheck.py
+++ emir/recipes/msmlsmcheck.py
@@ -23,3 +23,39 @@
 
     * MSM and LSM check
 '''
+
+import logging
+
+from numina.recipes import RecipeBase, Parameter, provides
+
+from ..dataproducts import MasterBias, MasterDark, MasterBadPixelMask
+from ..dataproducts import TelescopeOffset, MSMPositions
+from ..dataproducts import MasterIntensityFlat
+from ..dataproducts import NonLinearityCalibration
+
+__all__ = ['Recipe']
+
+_logger = logging.getLogger('emir.recipes')
+
+@provides(TelescopeOffset, MSMPositions)
+class Recipe(RecipeBase):
+    '''Acquire a target.'''
+
+    __requires__ = [       
+        Parameter('master_bias', MasterBias, 'Master bias image'),
+        Parameter('master_dark', MasterDark, 'Master dark image'),
+        Parameter('master_bpm', MasterBadPixelMask, 'Master bad pixel mask'),
+        Parameter('nonlinearity', NonLinearityCalibration([1.0, 0.0]), 
+                  'Polynomial for non-linearity correction'),
+        Parameter('master_intensity_ff', MasterIntensityFlat, 
+                  'Master intensity flatfield'),
+    ]
+
+    def __init__(self):
+        super(Recipe, self).__init__(
+            author="Sergio Pascual <sergiopr@fis.ucm.es>",
+            version="0.1.0"
+        )
+
+    def run(self, obresult):
+        return {'products': [TelescopeOffset(), MSMPositions()]}
