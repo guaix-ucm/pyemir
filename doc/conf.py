2d637d1cfc8960247d28df1a0f9adda085e100fc
Sergio Pascual <sergiopr@fis.ucm.es>
2012-09-13 12:55:35 +0200

    Added intersphinx configuration

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ sys.path.append(os.path.abspath('../src/'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.pngmath',
-              'sphinx.ext.coverage']
+              'sphinx.ext.coverage', 'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -194,3 +194,5 @@ latex_documents = [
 
 # A list of ignored prefixes for module index sorting.
 modindex_common_prefix = ['emir.']
+
+intersphinx_mapping = {'numina': ('http://packages.python.org/numina/', None)}
