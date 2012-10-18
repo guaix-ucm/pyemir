22100174490c1c3c7a24cc8b6cb5f53400bc13c3
Sergio Pascual <sergiopr@fis.ucm.es>
2012-10-18 19:29:42 +0200

    Returning correct variances in ramp

--- src/emir/preprocess.py
+++ src/emir/preprocess.py
@@ -19,6 +19,8 @@
 
 '''Preprocessing EMIR readout modes'''
 
+from __future__ import division
+
 import pyfits
 import numpy
 import math
@@ -135,16 +137,16 @@ def axis_ramp(data, badpix, img, var, nmap, mask, crmask,
         mask[...] = badpix[0]
     else:
         mm = data[data < saturation]
-        print 'axis ramp',mm
+
         if len(mm) <= 1:
             img[...] = blank
             var[...] = blank
             mask[...] = MASK_SATURATION
         else:
             v, vr, n, glt = ramp(mm, saturation, dt, gain, ron, nsig)
-            print v
+
             img[...] = v
-            print img[0]
+
             var[...] = vr
             mask[...] = MASK_GOOD
             nmap[...] = n
@@ -203,13 +205,13 @@ def preprocess_ramp(frame, saturation=65000, badpixels=None, blank=0,
         badpixels = numpy.zeros(frame[0].data[..., 0].shape, dtype='uint8')
 
     # time between samples
-    dt = elapsed / nsamples
+    dt = elapsed / (nsamples - 1)
 
     img, var, nmap, mask, crmask = loopover_ramp(frame[0].data, badpixels, saturation, dt, gain, ron, nsig)
 
-    frame[0].data = img
+    frame[0].data = img * elapsed
     frame[0].header['READPROC'] = True
-    varhdu = pyfits.ImageHDU(var)
+    varhdu = pyfits.ImageHDU(var * elapsed * elapsed)
     varhdu.update_ext_name('Variance')
     frame.append(varhdu)
     nmap = pyfits.ImageHDU(nmap)
@@ -222,6 +224,10 @@ def preprocess_ramp(frame, saturation=65000, badpixels=None, blank=0,
     crmaskhdu.update_ext_name('CRMASK')
     frame.append(crmaskhdu)
     return frame
+
+def loopovr_ramp(inpt, dt, gain, ron, badpixels=None, out=None, var=None, 
+                 saturation=60000, nsig=4.0, blank=0):
+    pass
 
 def loopover_ramp(data, badpixels, saturation, dt, gain, ron, nsig, blank=0):
 
@@ -271,16 +277,16 @@ def ramp(data, saturation, dt, gain, ron, nsig):
 
 # Finding glitches in the pixels
     intervals, glitches = rglitches(nsdata, gain=gain, ron=ron, nsig=nsig)
-    vals = numpy.asarray([slope(nsdata[intls], dt=dt, ron=ron) for intls in intervals if len(nsdata[intls]) >= 2])
+    vals = numpy.asarray([slope(nsdata[intls], dt=dt, gain=gain, ron=ron) for intls in intervals if len(nsdata[intls]) >= 2])
     weights = (1.0 / vals[:,1])
     average = numpy.average(vals[:,0], weights=weights)
-    variance = numpy.dot(weights, (vals[:,0] - average)**2) / weights.sum()
+    variance = 1.0 / weights.sum()
     return average, variance, vals[:,2].sum(), glitches
 
 def rglitches(nsdata, gain, ron, nsig):
     diffs = nsdata[1:] - nsdata[:-1]
     psmedian = numpy.median(diffs)
-    sigma = math.sqrt(psmedian / gain + 2 * ron * ron)
+    sigma = math.sqrt(abs(psmedian / gain) + 2 * ron * ron)
 
     start = 0
     intervals = []
@@ -296,21 +302,21 @@ def rglitches(nsdata, gain, ron, nsig):
     return intervals, glitches
 
 
-def slope(nsdata, dt, ron):
+def slope(nsdata, dt, gain, ron):
 
     if len(nsdata) < 2:
         raise ValueError('Two points needed to compute the slope')
 
-    FIXME_BASE = 10
-
     nn = len(nsdata)
-    delt = 12.0 / (nn * (nn + 1) * (nn - 1))
-    nf = (nn - 1) / 2.0
-
-    add = 0
-    for idx, d in enumerate(nsdata):
-        add += d * (idx - nf)
-    final = delt * add / dt
-    variance = FIXME_BASE + delt * ron * ron / dt / dt
-
+    delt = dt * nn * (nn + 1) * (nn - 1) / 12
+    ww = numpy.arange(1, nn + 1) - (nn + 1) / 2
+    
+    final = (ww * nsdata).sum() / delt
+    
+    # Readout limited case
+    delt2 = dt * delt
+    variance1 = (ron / gain)**2 / delt2
+    # Photon limiting case
+    variance2 = (6 * final * (nn * nn + 1)) / (5 * nn * dt * (nn * nn - 1) * gain)
+    variance = variance1 + variance2
     return final, variance, nn
