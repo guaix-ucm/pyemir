98655cd4a4b96becbac6879b1dac4648f6e52ee5
Sergio Pascual <sergiopr@fis.ucm.es>
2013-06-26 01:33:43 +0200

    Using a factory function to create EMIR_Instrument

--- lib/drp/emir/__init__.py
+++ lib/drp/emir/__init__.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2008-2012 Universidad Complutense de Madrid
+# Copyright 2008-2013 Universidad Complutense de Madrid
 # 
 # This file is part of PyEmir
 # 
@@ -20,85 +20,12 @@
 '''The EMIR Data Reduction Pipeline'''
 
 import logging
-import pkgutil
-import importlib
-    
-import yaml
-from numina.core import BaseInstrument, BasePipeline, InstrumentConfiguration
-from numina.core import import_object
 
-import emir.recipes as recp
-from emir.instrument.channels import convert_name_to_channels
-from emir import __version__
+from emir.instrument import sup1, sup2
 
 _logger = logging.getLogger('emir')
 
-_modes = [i for i in yaml.load_all(pkgutil.get_data('emir.instrument',
-                                                   'obsmodes.yaml'))
-          if i.instrument == 'EMIR']
+EMIR_Instrument = sup1()
 
-_equiv_class = {}
-
-for _m in _modes:
-    _m.recipe_class = import_object(_m.recipe)
-    _equiv_class[_m.key] = _m.recipe_class
-
-del _m
-        
-class EMIR_Pipeline(BasePipeline):
-    def __init__(self):
-        super(EMIR_Pipeline, self).__init__(name='default',
-                version=__version__,
-                recipes=_equiv_class)
-        
-class EMIR_Pipeline_ALT(BasePipeline):
-    def __init__(self):
-        super(EMIR_Pipeline_ALT, self).__init__(name='alternate',
-                version=__version__,
-                recipes=_equiv_class)
-
-_conf1 = InstrumentConfiguration('default', yaml.load(pkgutil.get_data('emir.instrument', 'default.yaml')))
-# converting
-_conf1 = convert_name_to_channels(_conf1)
-_logger.warning("default configuration for EMIR is broken")
-    
-_conf2 = InstrumentConfiguration('alternate', yaml.load(pkgutil.get_data('emir.instrument', 'alt.yaml')))
-_conf2 = convert_name_to_channels(_conf2)
-
-class EMIR_Instrument(BaseInstrument):
-    name = 'EMIR'
-    configurations = {'default': _conf1,
-                      'alt': _conf2}
-    if _conf2 is None:
-        del configurations['alt']
-    
-    pipelines = {'default': EMIR_Pipeline(),
-                'alternate': EMIR_Pipeline_ALT()}
-    modes = _modes
-
-    def __init__(self, configuration):
-        super(EMIR_Instrument, self).__init__('EMIR',
-                    configuration)
-
-_conf = InstrumentConfiguration('default', yaml.load(pkgutil.get_data('emir.instrument', 'default.yaml')))
-
-class EMIR_MUX_Pipeline(BasePipeline):
-    def __init__(self):
-        super(EMIR_MUX_Pipeline, self).__init__(name='default',
-                version=__version__, recipes={})
-
-class EMIR_MUX_Instrument(BaseInstrument):
-    name = 'EMIR_MUX'
-    configurations = {'default': _conf}
-    
-    pipelines = {'default': EMIR_MUX_Pipeline()}
-    modes = [i for i in yaml.load_all(pkgutil.get_data('emir.instrument',
-                                                   'obsmodesmux.yaml'))
-          if i.instrument == 'EMIR_MUX']
-
-    def __init__(self, configuration):
-        super(EMIR_MUX_Instrument, self).__init__('EMIR_MUX',
-                    configuration)
-
-del _modes
+EMIR_MUX_Instrument = sup2()
 
--- lib/emir/instrument/__init__.py
+++ lib/emir/instrument/__init__.py
@@ -16,3 +16,89 @@
 # You should have received a copy of the GNU General Public License
 # along with PyEmir.  If not, see <http://www.gnu.org/licenses/>.
 # 
+
+import yaml
+from numina.core import BaseInstrument, BasePipeline, InstrumentConfiguration
+from numina.core import import_object
+import logging
+import pkgutil
+
+import emir.recipes as recp
+from emir.instrument.channels import convert_name_to_channels
+from emir import __version__
+
+_logger = logging.getLogger('emir')
+
+def _load_modes():
+    equiv_class = {}
+    modes = [i for i in yaml.load_all(pkgutil.get_data('emir.instrument',
+                                                   'obsmodes.yaml'))
+          if i.instrument == 'EMIR']
+    for m in modes:
+        m.recipe_class = import_object(m.recipe)
+        equiv_class[m.key] = m.recipe_class
+    return modes, equiv_class
+
+def sup1():
+
+    _modes, equiv_class = _load_modes()
+
+    class EMIR_Pipeline(BasePipeline):
+        def __init__(self):
+            super(EMIR_Pipeline, self).__init__(name='default',
+                    version=__version__,
+                    recipes=equiv_class)
+        
+    class EMIR_Pipeline_ALT(BasePipeline):
+        def __init__(self):
+            super(EMIR_Pipeline_ALT, self).__init__(name='alternate',
+                    version=__version__,
+                    recipes=equiv_class)
+
+    _conf1 = InstrumentConfiguration('default', yaml.load(pkgutil.get_data('emir.instrument', 'default.yaml')))
+    # converting
+    _conf1 = convert_name_to_channels(_conf1)
+    
+    _conf2 = InstrumentConfiguration('alternate', yaml.load(pkgutil.get_data('emir.instrument', 'alt.yaml')))
+    _conf2 = convert_name_to_channels(_conf2)
+
+    class EMIR_Instrument(BaseInstrument):
+        name = 'EMIR'
+        configurations = {'default': _conf1,
+                      'alt': _conf2}
+        if _conf2 is None:
+            del configurations['alt']
+    
+        pipelines = {'default': EMIR_Pipeline(),
+                    'alternate': EMIR_Pipeline_ALT()}
+        modes = _modes
+
+        def __init__(self, configuration):
+            super(EMIR_Instrument, self).__init__('EMIR',
+                    configuration)
+
+    return EMIR_Instrument
+
+def sup2():
+    _conf = InstrumentConfiguration('default', yaml.load(pkgutil.get_data('emir.instrument', 'default.yaml')))
+
+    class EMIR_MUX_Pipeline(BasePipeline):
+        def __init__(self):
+            super(EMIR_MUX_Pipeline, self).__init__(name='default',
+                    version=__version__, recipes={})
+
+    class EMIR_MUX_Instrument(BaseInstrument):
+        name = 'EMIR_MUX'
+        configurations = {'default': _conf}
+    
+        pipelines = {'default': EMIR_MUX_Pipeline()}
+        modes = [i for i in yaml.load_all(pkgutil.get_data('emir.instrument',
+                                                   'obsmodesmux.yaml'))
+            if i.instrument == 'EMIR_MUX']
+
+        def __init__(self, configuration):
+            super(EMIR_MUX_Instrument, self).__init__('EMIR_MUX',
+                    configuration)
+
+    return EMIR_MUX_Instrument
+
