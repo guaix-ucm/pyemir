d596933301cdcf1a6111104bbd6e95a137df21d3
Sergio Pascual <sergiopr@fis.ucm.es>
2013-10-28 16:13:08 +0100

    Import preprocess from emir-tasks

--- lib/emir/preprocess.py
+++ lib/emir/preprocess.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2008-2012 Universidad Complutense de Madrid
+# Copyright 2008-2013 Universidad Complutense de Madrid
 # 
 # This file is part of PyEmir
 # 
@@ -19,13 +19,100 @@
 
 '''Preprocessing EMIR readout modes'''
 
+from __future__ import print_function
 from __future__ import division
 
 import pyfits
-import numpy
-import math
 
-from numina.array import ramp_array
+from numina.array import ramp_array, fowler_array
+
+PREPROC_KEY = 'READPROC'
+PREPROC_VAL = True
+
+class ReadModeGuessing(object):
+    def __init__(self, mode, info=None):
+        self.mode = mode
+        self.info = info
+
+def image_readmode(hdulist, default=None):
+    header = hdulist[0].header
+    if 'READMODE' in header:
+        p_readmode = header['READMODE'].lower()
+        if p_readmode in ['single', 'cds', 'fowler', 'ramp']:
+            return ReadModeGuessing(mode=p_readmode, info={'source': 'keyword'})
+    # Using heuristics
+    shape = hdulist[0].shape
+    if len(shape) == 2:
+        # A 2D image, mode is single
+        return ReadModeGuessing(mode='single', info={'source': 'heuristics'})
+    else:
+        nd = min(shape)
+        if nd == 2:
+            # A NXNX2 image
+            # mide is cds
+            return ReadModeGuessing(mode='cds', info={'source': 'heuristics'})
+    # Insufficient information
+    if default:
+        return ReadModeGuessing(mode=default, info={'source': 'default'})
+    else:
+        None
+
+def preprocess_single(hdulist):
+    return hdulist
+
+def preprocess_cds(hdulist):
+    # CDS is Fowler with just one pair of reads
+    return preprocess_fowler(hdulist)
+
+def preprocess_fowler(hdulist):
+    hdulist[0].header.update(PREPROC_KEY, PREPROC_VAL)
+    # We need:
+    tint = 0.0 # Integration time (from first read to last read)
+    ts = 0.0 # Time between samples
+    gain = 1.0 # Detector gain (number)
+    ron = 1.0 # Detector RON (number)
+    # A master badpixel mask
+    cube = hdulist[0].data
+
+    res, var, npix, mask = fowler_array(cube, ti=tint, ts=ts, 
+        gain=gain, ron=ron, badpixels=None, dtype='float32',
+        saturation=55000.0)
+    hdulist[0].data = res
+    varhdu = pyfits.ImageHDU(var)
+    varhdu.update_ext_name('VARIANCE')
+    hdulist.append(varhdu)
+    nmap = pyfits.ImageHDU(npix)
+    nmap.update_ext_name('MAP')
+    hdulist.append(nmap)
+    nmask_hdu = pyfits.ImageHDU(mask)
+    nmask_hdu.update_ext_name('MASK')
+    hdulist.append(nmask_hdu)
+    return hdulist
+
+def preprocess_ramp(hdulist):
+    hdulist[0].header.update(PREPROC_KEY, PREPROC_VAL)
+    cube = hdulist[0].data
+    # We need
+    ti = 0.0 # Integration time
+    gain = 1.0
+    ron = 1.0
+    rslt = ramp_array(cube, ti,
+        gain=gain, ron=ron, badpixels=None, dtype='float32',
+        saturation=55000.0)
+    result, var, npix, mask = rslt
+
+    hdulist[0].data = res
+    varhdu = pyfits.ImageHDU(var)
+    varhdu.update_ext_name('VARIANCE')
+    hdulist.append(varhdu)
+
+    nmap = pyfits.ImageHDU(npix)
+    nmap.update_ext_name('MAP')
+    hdulist.append(nmap)
+    nmask = pyfits.ImageHDU(mask)
+    nmask.update_ext_name('MASK')
+    hdulist.append(nmask)
+    return hdulist
 
 def fits_wrapper(frame):
     if isinstance(frame, basestring):
@@ -34,167 +121,31 @@ def fits_wrapper(frame):
         return frame
     else:
         raise TypeError
-        
-    
-def preprocess(frame):
-    with fits_wrapper(frame) as hdu:
-        header = hdu[0].header
-        
-        if header.get('READPROC'):
-            return frame
-        
-        readmode = header.get('READMODE', "") == 'SINGLE'
-        
-        if readmode == 'SINGLE': 
-            pass
-        elif readmode == 'CDS':
-            pass
-        elif readmode == 'FOWLER':
-            pass
-        elif readmode == 'RAMP':
+
+def preprocess(input, output):
+    with fits_wrapper(input) as hdulist:
+        header = hdulist[0].header
+        if 'PREPROC' in header:
+            # if the image is preprocessed, do nothing
+            if input != output:
+                hdulist.writeto(output, clobber=True)
+            return
+        # determine the READ mode
+        guess = image_readmode(hdulist, 'single')
+        if guess is None:
+            # We have a problem here
+            return
+
+        if guess.mode == 'single':
+            hduproc = preprocess_single(hdulist)
+        elif guess.mode == 'cds':
+            hduproc = preprocess_cds(hdulist)
+        elif guess.mode == 'fowler':
+            hduproc = preprocess_fowler(hdulist)
+        elif guess.mode == 'ramp':
             pass
         else:
-            raise ValueError
-
-def preprocess_single(frame, saturation=65536, badpixels=None, blank=0):
-    pass
-    
-
-def preprocess_cds(frame):
-    
-    samp1 = frame[0].data[..., 0]
-    samp2 = frame[0].data[..., 1]
-    final = samp2 - samp1
-    frame[0].data = final
-    frame[0].header['READPROC'] = True
-    return frame
-
-def preprocess_fowler(frame, saturation=65536, badpixels=None, blank=0):
-
-    if frame[0].header['readmode'] != 'FOWLER':
-        raise ValueError('Frame is not in Fowler mode')
-
-
-    # We are assuming here that the 3rd axis is the number of frames
-    shape = frame[0].shape
-    # This construction could be handled inside loopover_fowler
-    if badpixels is None:
-        badpixels = numpy.zeros(shape[0:-1], dtype='>u1')
-        
-    hsize = shape[2] // 2
-    if 2 * hsize != shape[2]:
-        raise ValueError('Number of samples must be an even number')
-
-    img, var, nmap, mask = loopover_fowler(frame[0].data, badpixels, saturation, hsize, blank=blank)
-
-    frame[0].data = img
-    frame[0].header['READPROC'] = True
-    varhdu = pyfits.ImageHDU(var)
-    varhdu.update_ext_name('Variance')
-    frame.append(varhdu)
-    nmap = pyfits.ImageHDU(nmap)
-    nmap.update_ext_name('MAP')
-    frame.append(nmap)
-    nmask = pyfits.ImageHDU(mask)
-    nmask.update_ext_name('MASK')
-    frame.append(nmask)
-    return frame
-
-def axis_fowler(data, badpix, img, var, nmap, mask, hsize, saturation, blank=0):
-    '''Apply Fowler processing to a series of data.'''
-    MASK_SATURATION = 3 
-    MASK_GOOD = 0
-     
-    if badpix[0] != MASK_GOOD:
-        img[...] = blank
-        var[...] = blank
-        mask[...] = badpix[0]
-    else:
-        mm = numpy.asarray([(b - a) for a, b in zip(data[:hsize], data[hsize:]) if b < saturation and a < saturation])
-        npix = len(mm)
-        nmap[...] = npix
-        if npix == 0:
-            img[...] = blank
-            var[...] = blank
-            mask[...] = MASK_SATURATION
-        elif npix == 1:
-            img[...] = mm.mean()
-            var[...] = blank
-            mask[...] = MASK_GOOD
-        else:
-            img[...] = mm.mean()
-            var[...] = mm.var() / npix
-            mask[...] = MASK_GOOD
-
-
-def loopover_fowler(data, badpixels, saturation, hsize, blank=0):
-    '''Loop over the 3d array applying Fowler processing.'''
-    imgfin = None
-    varfin = None
-
-#    imgfin = numpy.empty(badpixels.shape, dtype='>i2') # int16, bigendian
-#    varfin = numpy.empty(badpixels.shape, dtype='>i2') # int16, bigendian
-
-    nmask = numpy.empty(badpixels.shape, dtype='>u1') # uint8, bigendian
-    npixmask = numpy.empty(badpixels.shape, dtype='>u1') # uint8, bigendian
-
-    it = numpy.nditer([data, badpixels, imgfin, varfin, npixmask, nmask], 
-                flags=['reduce_ok', 'external_loop',
-                    'buffered', 'delay_bufalloc'],
-                    op_flags=[['readonly'], ['readonly', 'no_broadcast'], 
-                            ['readwrite', 'allocate'], 
-                            ['readwrite', 'allocate'],
-                            ['readwrite', 'allocate'],
-                            ['readwrite', 'allocate'], 
-                            ],
-                    op_axes=[None,
-                            [0,1,-1], 
-                            [0,1,-1],
-                            [0,1,-1], 
-                            [0,1,-1], 
-                            [0,1,-1]
-                           ])
-    for i in range(2, 6):
-        it.operands[i][...] = 0
-    it.reset()
-
-    for x, badpix, img, var, nmap, mask in it:
-        axis_fowler(x, badpix, img, var, nmap, mask, hsize, saturation, blank=blank)
-
-    # Building final frame
-    return tuple(it.operands[i] for i in range(2, 6))
-
-def preprocess_ramp(frame, gain, ron, badpixels=None, saturation=60000, nsig=4.0, blank=0, dtype='float32'):
-
-    if 'readproc' in frame[0].header and frame[0].header['readproc']:
-        # Already processsed
-        return frame
-
-    if frame[0].header['readmode'].upper() != 'RAMP':
-        raise ValueError('Frame is not in RAMP mode')
-
-    elapsed = frame[0].header['exposed']
-    nsamples = frame[0].header['readsamp']
+            hduproc = preprocess_single(hdulist)
+            
+        hduproc.writeto(output, clobber=True)
  
-    # time between samples
-    dt = elapsed / (nsamples - 1)
-
-    img, var, nmap, mask, crmask = ramp_array(frame[0].data, dt, gain, ron, 
-        saturation=saturation, nsig=nsig, blank=blank, dtype=dtype)
-
-    frame[0].data = img * elapsed
-    frame[0].header.update('READPROC', True)
-    varhdu = pyfits.ImageHDU(var * elapsed * elapsed)
-    varhdu.update_ext_name('Variance')
-    frame.append(varhdu)
-    nmap = pyfits.ImageHDU(nmap)
-    nmap.update_ext_name('MAP')
-    frame.append(nmap)
-    nmask = pyfits.ImageHDU(mask)
-    nmask.update_ext_name('MASK')
-    frame.append(nmask)
-    crmaskhdu = pyfits.ImageHDU(crmask)
-    crmaskhdu.update_ext_name('CRMASK')
-    frame.append(crmaskhdu)
-    return frame
-
