450d5f21e87af7211d666dd057df57280b416d8c
Sergio Pascual <sergiopr@fis.ucm.es>
2014-03-13 13:35:21 +0000

    Correct import for dialect

--- lib/emir/ext/gtc/__init__.py
+++ lib/emir/ext/gtc/__init__.py
@@ -1,14 +1,14 @@
 
-import Whatever
+import DF
 
 
 from numina.ext.gtc import register
 
 _more = {
-'emir.dataproducts.MasterBias': 'DPK.DPKFrame',
-'emir.dataproducts.MasterDark': 'DPK.FRame',
-'emir.dataproducts.MasterIntensityFlat': 'DPK.FRame', 
-'emir.dataproducts.CoordinateList2DType': 'DPK.Array2D',
+'emir.dataproducts.MasterBias': DF.TYPE_FRAME,
+'emir.dataproducts.MasterDark': DF.TYPE_FRAME,
+'emir.dataproducts.MasterIntensityFlat': DF.TYPE_FRAME, 
+'emir.dataproducts.CoordinateList2DType': DF.TYPE_DOUBLE_ARRAY2D,
 }
 
 register(_more)
