c1e099f2cee29a5322ff057591bfa2b8cda95d3c
Sergio Pascual <sergiopr@fis.ucm.es>
2014-03-13 04:48:44 +0100

    Missing ,

--- lib/emir/ext/gtc/__init__.py
+++ lib/emir/ext/gtc/__init__.py
@@ -5,7 +5,7 @@ import Whatever
 from numina.ext.gtc import register
 
 _more = {
-'emir.dataproducts.MasterBias': 'DPK.DPKFrame'
+'emir.dataproducts.MasterBias': 'DPK.DPKFrame',
 'emir.dataproducts.MasterDark': 'DPK.FRame',
 'emir.dataproducts.MasterIntensityFlat': 'DPK.FRame', 
 'emir.dataproducts.CoordinateList2DType': 'DPK.Array2D',
