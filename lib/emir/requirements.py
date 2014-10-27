b9904040b759d91e745ed88072ea4cf938893b5f
Sergio Pascual <sergiopr@fis.ucm.es>
2014-10-27 12:50:30 +0100

    Add PEP8 fixes

--- lib/emir/requirements.py
+++ lib/emir/requirements.py
@@ -1,21 +1,21 @@
 #
 # Copyright 2008-2014 Universidad Complutense de Madrid
-# 
+#
 # This file is part of PyEmir
-# 
+#
 # PyEmir is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # PyEmir is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with PyEmir.  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 
 '''Typical requirements of recipes'''
 
@@ -24,62 +24,89 @@ from numina.core import Parameter, DataProductRequirement, Requirement
 from emir.dataproducts import MasterBias, MasterDark, MasterBadPixelMask
 from emir.dataproducts import MasterIntensityFlat
 from emir.dataproducts import MasterSpectralFlat
-#from emir.dataproducts import SourcesCatalog
+# from emir.dataproducts import SourcesCatalog
 from emir.dataproducts import EMIRConfigurationType
+
 
 class EMIRConfigurationRequirement(Requirement):
     '''The Recipe requires the configuration of EMIR.'''
     def __init__(self):
-        
-        super(EMIRConfigurationRequirement, self).__init__("EMIR Configuration", 
-            type=EMIRConfigurationType, validate=True)
+
+        super(EMIRConfigurationRequirement,
+              self).__init__("EMIR Configuration",
+                             type=EMIRConfigurationType,
+                             validate=True
+                             )
 
     def __repr__(self):
         sclass = type(self).__name__
-        return "%s(dest=%r, description='%s')" % (sclass, self.dest, self.description)
+        return "%s(dest=%r, description='%s')" % (sclass,
+                                                  self.dest,
+                                                  self.description
+                                                  )
 
 
 class MasterBadPixelMaskRequirement(DataProductRequirement):
     def __init__(self):
-        super(MasterBadPixelMaskRequirement, self).__init__(MasterBadPixelMask, 
-                                                            'Master bad pixel mask')
+        super(MasterBadPixelMaskRequirement,
+              self).__init__(MasterBadPixelMask,
+                             'Master bad pixel mask'
+                             )
+
 
 class MasterBiasRequirement(DataProductRequirement):
     def __init__(self, optional=True):
-        super(MasterBiasRequirement, self).__init__(MasterBias, 'Master BIAS image', 
-                                                    optional=optional)
-        
+        super(MasterBiasRequirement,
+              self).__init__(MasterBias,
+                             'Master BIAS image',
+                             optional=optional
+                             )
+
+
 class MasterDarkRequirement(DataProductRequirement):
     def __init__(self):
-        super(MasterDarkRequirement, self).__init__(MasterDark, 'Master DARK image')
-        
+        super(MasterDarkRequirement,
+              self).__init__(MasterDark, 'Master DARK image')
+
+
 class MasterIntensityFlatFieldRequirement(DataProductRequirement):
     def __init__(self):
-        super(MasterIntensityFlatFieldRequirement, 
+        super(MasterIntensityFlatFieldRequirement,
               self).__init__(MasterIntensityFlat, 'Master intensity flatfield')
-              
-              
+
+
 class MasterSpectralFlatFieldRequirement(DataProductRequirement):
     def __init__(self):
-        super(MasterSpectralFlatFieldRequirement, 
+        super(MasterSpectralFlatFieldRequirement,
               self).__init__(MasterSpectralFlat, 'Master spectral flatfield')
+
 
 class Extinction_Requirement(Parameter):
     def __init__(self):
-        super(Extinction_Requirement, self).__init__(0.0, 'Mean atmospheric extinction')
-        
+        super(Extinction_Requirement, self).__init__(
+            0.0, 'Mean atmospheric extinction'
+            )
+
+
 class SkyImageSepTime_Requirement(Parameter):
     def __init__(self):
-        super(SkyImageSepTime_Requirement, self).__init__(10.0, 
-            'Maximum time interval between target and sky images [minutes]')
-        
+        super(SkyImageSepTime_Requirement, self).__init__(
+            10.0,
+            'Maximum time interval between target and sky images [minutes]'
+            )
+
+
 class Catalog_Requirement(Parameter):
     def __init__(self, optional=True):
-        super(Catalog_Requirement, self).__init__([], 
-              'List of x, y coordinates to measure FWHM',
-              optional=optional)
-        
+        super(Catalog_Requirement, self).__init__(
+            [], 'List of x, y coordinates to measure FWHM',
+            optional=optional
+            )
+
+
 class Offsets_Requirement(Parameter):
     def __init__(self, optional=True):
-        super(Offsets_Requirement, self).__init__([], 'List of pairs of offsets',
-                                                  optional=optional)
+        super(Offsets_Requirement, self).__init__(
+            [], 'List of pairs of offsets',
+            optional=optional
+            )
