63968a4e94ed71d681810e0a1259ad3a94af01bf
Sergio Pascual <sergiopr@fis.ucm.es>
2014-10-27 11:49:05 +0100

    Add PEP8 fixes

--- lib/emir/ext/gtc/__init__.py
+++ lib/emir/ext/gtc/__init__.py
@@ -2,13 +2,11 @@
 try:
     import DF
 
-
     from numina.ext.gtc import register
 
-    _more = {
-             'emir.dataproducts.MasterBias': DF.TYPE_FRAME,
+    _more = {'emir.dataproducts.MasterBias': DF.TYPE_FRAME,
              'emir.dataproducts.MasterDark': DF.TYPE_FRAME,
-             'emir.dataproducts.MasterIntensityFlat': DF.TYPE_FRAME, 
+             'emir.dataproducts.MasterIntensityFlat': DF.TYPE_FRAME,
              'emir.dataproducts.CoordinateList2DType': DF.TYPE_DOUBLE_ARRAY2D,
              }
     register(_more)
