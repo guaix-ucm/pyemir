3cffa70a804a3e4fb9f8bdf1420ba22627502e92
Sergio Pascual <sergiopr@fis.ucm.es>
2015-06-19 16:37:08 +0200

    Functional implementation of TEST10

--- lib/emir/recipes/aiv/csua.py
+++ lib/emir/recipes/aiv/csua.py
@@ -28,11 +28,13 @@ from scipy import ndimage
 from scipy.ndimage.filters import median_filter
 from skimage.filter import canny
 
-from numina.core import Product, Parameter
+from numina.core import Product, Parameter, Requirement
 from numina.core.requirements import ObservationResultRequirement
 from numina.core import RecipeError
+from numina.array.utils import image_box2d
 #
 from emir.core import EmirRecipe
+from emir.dataproducts import CoordinateList2DType
 from emir.dataproducts import DataFrameType
 from emir.dataproducts import ArrayType
 from emir.requirements import MasterBiasRequirement
@@ -48,6 +50,57 @@ from .common import get_dtur_from_header
 _logger = logging.getLogger('numina.recipes.emir')
 
 
+def slit_alignment(img, x, y, canny_sigma=5):
+    
+    # size of the cut box used to analyze the slit
+    hbox_x = 50
+    hbox_y = 75
+    
+    # Remove nstrip rows on top and bottom
+    # before computing the center of mass
+    nstrip = 30
+    
+    # Fill bfill columns before doing the binary fill
+    bfill = 2
+    
+    # Region of a box, 50 *2  pixels in X, 75 *2 pixels in Y
+    roi = image_box2d(x, y, img.shape, (hbox_y, hbox_x))
+    
+    mm = img[roi]
+    
+    edges = canny(mm, sigma=canny_sigma)
+    
+    # Fill edges
+    # Fill columns in the borders to help closing the bars
+    edges[:,:bfill] = 1
+    edges[:,-bfill:] = 1
+    fill_slits =  ndimage.binary_fill_holes(edges)
+    
+    # Reverse image
+    vv = (mm) * (1 - fill_slits)
+
+    # Cut nstrip rows
+    vv2 = vv[nstrip:-nstrip]
+    
+    # Center of mass in X
+    XX = numpy.arange(vv2.shape[1])
+    YY = numpy.arange(vv2.shape[0])
+    
+    SF1 = vv2.sum(axis=1)
+    XB = (vv2 * XX).sum(axis=1) / SF1
+    
+    SF = SF1.sum()
+    XM = (vv2 * XX).sum() / SF
+    YM = (vv2 * YY[:,None]).sum() / SF # Broadcasting here YY
+
+    bb = numpy.polyfit(YY, XB, deg=1)
+    
+    XM += roi[1].start
+    YM += roi[0].start + nstrip
+    
+    return [XM, YM], bb
+
+
 class TestCSUAlignmentRecipe(EmirRecipe):
 
     # Recipe Requirements
@@ -57,7 +110,12 @@ class TestCSUAlignmentRecipe(EmirRecipe):
     master_flat = MasterIntensityFlatFieldRequirement()
     master_sky = MasterSkyRequirement()
 
-    median_filter_size = Parameter(4, 'Size of the median box')
+    slits_positions = Requirement(CoordinateList2DType,
+                                  'Positions of the center of the slits'
+                                 )
+
+    median_filter_size = Parameter(5, 'Size of the median box')
+    canny_sigma = Parameter(5.0, 'Sigma for the canny algorithm')
 
     # Recipe Results
     frame = Product(DataFrameType)
@@ -74,17 +132,27 @@ class TestCSUAlignmentRecipe(EmirRecipe):
         hdr = hdulist[0].header
         self.set_base_headers(hdr)
 
-        _logger.debug('finding slits')
-
         # First, prefilter with median
         median_filter_size = rinput.median_filter_size
-        
         
         data1 = hdulist[0].data
         _logger.debug('Median filter with box %d', median_filter_size)
         data2 = median_filter(data1, size=median_filter_size)
 
-        result = self.create_result(frame=hdulist, slitstable=[1])
+        nresults = 4
+        slits_t = numpy.empty((rinput.slits_positions.shape[0], nresults))
+        # Work with slits
+        for idx, (x, y) in enumerate(rinput.slits_positions):
+            _logger.debug('processing slit in %f %f', x-1, y-1)
+            centroid, fit = slit_alignment(data2, x - 1, y -1,
+                                           rinput.canny_sigma)
+            slits_t[idx, :2] = centroid
+            slits_t[idx, 2:] = fit
+
+        # FITS coordinates
+        slits_t[:,:2] += 1
+
+        result = self.create_result(frame=hdulist, slitstable=slits_t)
 
         return result
 
