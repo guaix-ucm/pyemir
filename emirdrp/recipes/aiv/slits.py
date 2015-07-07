817b2939d80d7e38a855c482b14a1d2628a406dc
Sergio Pascual <sergiopr@fis.ucm.es>
2015-07-07 17:52:51 +0200

    New recipe for slits in the CSU

--- emirdrp/recipes/aiv/slits.py
+++ emirdrp/recipes/aiv/slits.py
@@ -60,11 +60,8 @@ class TestSlitDetectionRecipe(EmirRecipe):
     master_flat = MasterIntensityFlatFieldRequirement()
     master_sky = MasterSkyRequirement()
 
-    median_filter_size = Parameter(4, 'Size of the median box')
+    median_filter_size = Parameter(5, 'Size of the median box')
     canny_sigma = Parameter(3.0, 'Sigma for the canny algorithm')
-    obj_min_size = Parameter(200, 'Minimum size of the slit')
-    obj_max_size = Parameter(3000, 'Maximum size of the slit')
-    slit_size_ratio = Parameter(4.0, 'Minimum ratio between height and width for slits')
 
     # Recipe Results
     frame = Product(DataFrameType)
@@ -97,14 +94,17 @@ class TestSlitDetectionRecipe(EmirRecipe):
 
         _logger.debug('finding slits')
 
+
+        # Filter values below 0.0
+        _logger.debug('Filter values below 0')
+        data1 = hdulist[0].data[:]
+
+        data1[data1 < 0.0] = 0.0
         # First, prefilter with median
         median_filter_size = rinput.median_filter_size
         canny_sigma = rinput.canny_sigma
-        obj_min_size = rinput.obj_min_size
-        obj_max_size = rinput.obj_max_size
         
         
-        data1 = hdulist[0].data
         _logger.debug('Median filter with box %d', median_filter_size)
         data2 = median_filter(data1, size=median_filter_size)
 
@@ -117,49 +117,29 @@ class TestSlitDetectionRecipe(EmirRecipe):
         
         # Fill edges
         _logger.debug('Fill holes')
-        fill_slits =  ndimage.binary_fill_holes(edges)
+        # I do a dilation and erosion to fill
+        # possible holes in 'edges'
+        fill = ndimage.binary_dilation(edges)
+        fill2 = ndimage.binary_fill_holes(fill)
+        fill_slits = ndimage.binary_erosion(fill2)
 
         _logger.debug('Label objects')
         label_objects, nb_labels = ndimage.label(fill_slits)
         _logger.debug('%d objects found', nb_labels)
-        # Filter on the area of the labeled region
-        # Perhaps we could ignore this filtering and
-        # do it later?
-        _logger.debug('Filter objects by size')
-        # Sizes of regions
-        sizes = numpy.bincount(label_objects.ravel())
-
-        _logger.debug('Min size is %d', obj_min_size)
-        _logger.debug('Max size is %d', obj_max_size)
-
-        mask_sizes = (sizes > obj_min_size) & (sizes < obj_max_size)
-
-        # Filter out regions
-        nids, = numpy.where(mask_sizes)
-
-        mm = numpy.in1d(label_objects, nids)
-        mm.shape = label_objects.shape
-
-        fill_slits_clean = numpy.where(mm, 1, 0)
-        #plt.imshow(fill_slits_clean)
-
-        # and relabel
-        _logger.debug('Label filtered objects')
-        relabel_objects, nb_labels = ndimage.label(fill_slits_clean)
-        _logger.debug('%d objects found after filtering', nb_labels)
         ids = list(six.moves.range(1, nb_labels + 1))
 
         _logger.debug('Find regions and centers')
-        regions = ndimage.find_objects(relabel_objects)
-        centers = ndimage.center_of_mass(data2, labels=relabel_objects,
+        regions = ndimage.find_objects(label_objects)
+        centers = ndimage.center_of_mass(data2, labels=label_objects,
                                          index=ids
                                          )
 
         table = char_slit(data2, regions, centers,
-                          slit_size_ratio=rinput.slit_size_ratio
+                          slit_size_ratio=-1.0
                           )
 
-        result = self.create_result(frame=hdulist, slitstable=table,
+        result = self.create_result(frame=hdulist,
+                                    slitstable=table,
                                     DTU=dtur,
                                     IPA=ipa,
                                     DETPA=detpa,
