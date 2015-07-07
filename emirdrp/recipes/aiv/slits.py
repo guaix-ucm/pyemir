dae4c9a4f162d4d2e48fa4ef2eafb946519e02b7
Sergio Pascual <sergiopr@fis.ucm.es>
2015-07-07 16:09:33 +0200

    Rename recipe used to detect slits in masks

--- emirdrp/recipes/aiv/slits.py
+++ emirdrp/recipes/aiv/slits.py
@@ -168,3 +168,121 @@ class TestSlitDetectionRecipe(EmirRecipe):
 
         return result
 
+
+class TestSlitMaskDetectionRecipe(EmirRecipe):
+
+    # Recipe Requirements
+    obresult = ObservationResultRequirement()
+    master_bias = MasterBiasRequirement()
+    master_dark = MasterDarkRequirement()
+    master_flat = MasterIntensityFlatFieldRequirement()
+    master_sky = MasterSkyRequirement()
+
+    median_filter_size = Parameter(4, 'Size of the median box')
+    canny_sigma = Parameter(3.0, 'Sigma for the canny algorithm')
+    obj_min_size = Parameter(200, 'Minimum size of the slit')
+    obj_max_size = Parameter(3000, 'Maximum size of the slit')
+    slit_size_ratio = Parameter(4.0, 'Minimum ratio between height and width for slits')
+
+    # Recipe Results
+    frame = Product(DataFrameType)
+    slitstable = Product(ArrayType)
+    DTU = Product(ArrayType)
+    IPA = Product(float)
+    DETPA = Product(float)
+    DTUPA = Product(float)
+
+    def run(self, rinput):
+        _logger.info('starting slit processing')
+
+        _logger.info('basic image reduction')
+
+        flow = init_filters_bdfs(rinput)
+
+        hdulist = basic_processing_with_combination(rinput, flow=flow)
+        hdr = hdulist[0].header
+        self.set_base_headers(hdr)
+
+        try:
+            ipa = hdr['IPA']
+            detpa = hdr['DETPA']
+            dtupa = hdr['DTUPA']
+            dtur = get_dtur_from_header(hdr)
+
+        except KeyError as error:
+            _logger.error(error)
+            raise RecipeError(error)
+
+        _logger.debug('finding slits')
+
+        # First, prefilter with median
+        median_filter_size = rinput.median_filter_size
+        canny_sigma = rinput.canny_sigma
+        obj_min_size = rinput.obj_min_size
+        obj_max_size = rinput.obj_max_size
+        
+        
+        data1 = hdulist[0].data
+        _logger.debug('Median filter with box %d', median_filter_size)
+        data2 = median_filter(data1, size=median_filter_size)
+
+        # Grey level image
+        img_grey = normalize(data2)
+
+        # Find edges with canny
+        _logger.debug('Find edges with canny, sigma %d', canny_sigma)
+        edges = canny(img_grey, sigma=canny_sigma)
+        
+        # Fill edges
+        _logger.debug('Fill holes')
+        fill_slits =  ndimage.binary_fill_holes(edges)
+
+        _logger.debug('Label objects')
+        label_objects, nb_labels = ndimage.label(fill_slits)
+        _logger.debug('%d objects found', nb_labels)
+        # Filter on the area of the labeled region
+        # Perhaps we could ignore this filtering and
+        # do it later?
+        _logger.debug('Filter objects by size')
+        # Sizes of regions
+        sizes = numpy.bincount(label_objects.ravel())
+
+        _logger.debug('Min size is %d', obj_min_size)
+        _logger.debug('Max size is %d', obj_max_size)
+
+        mask_sizes = (sizes > obj_min_size) & (sizes < obj_max_size)
+
+        # Filter out regions
+        nids, = numpy.where(mask_sizes)
+
+        mm = numpy.in1d(label_objects, nids)
+        mm.shape = label_objects.shape
+
+        fill_slits_clean = numpy.where(mm, 1, 0)
+        #plt.imshow(fill_slits_clean)
+
+        # and relabel
+        _logger.debug('Label filtered objects')
+        relabel_objects, nb_labels = ndimage.label(fill_slits_clean)
+        _logger.debug('%d objects found after filtering', nb_labels)
+        ids = list(six.moves.range(1, nb_labels + 1))
+
+        _logger.debug('Find regions and centers')
+        regions = ndimage.find_objects(relabel_objects)
+        centers = ndimage.center_of_mass(data2, labels=relabel_objects,
+                                         index=ids
+                                         )
+
+        table = char_slit(data2, regions, centers,
+                          slit_size_ratio=rinput.slit_size_ratio
+                          )
+
+        result = self.create_result(frame=hdulist, slitstable=table,
+                                    DTU=dtur,
+                                    IPA=ipa,
+                                    DETPA=detpa,
+                                    DTUPA=dtupa
+                                    )
+
+        return result
+
