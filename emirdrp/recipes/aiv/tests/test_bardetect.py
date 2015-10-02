a5d5c28f3e75e94edd5670a8891f9536f96e5569
Sergio Pascual <sergiopr@fis.ucm.es>
2015-10-02 16:57:41 +0200

    Add simple test for find_position

--- emirdrp/recipes/aiv/tests/test_bardetect.py
+++ emirdrp/recipes/aiv/tests/test_bardetect.py
@@ -3,6 +3,8 @@ import pytest
 import numpy
 
 from ..bardetect import calc_fwhm
+from ..bardetect import find_position
+
 
 @pytest.mark.parametrize("axis, result", [(0,7), (1, 16)])
 def test_calc_fwhm(axis, result):
@@ -13,3 +15,17 @@ def test_calc_fwhm(axis, result):
     fwhm = calc_fwhm(img, region, axis=axis)
     assert fwhm == result
 
+
+def test_find_position():
+
+    edges = numpy.zeros((100, 100), dtype='int')
+    edges[5:12,40] = 1
+    edges[5:12,45] = 1
+
+    # Find slit
+    pos = find_position(edges, 8.0, 1, 99)
+    assert pos == (8, 40.0, 45.0)
+
+    # Dont find it
+    pos = find_position(edges, 40, 1, 99)
+    assert pos is None
