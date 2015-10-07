9ca83bb71d9d1ae036083bdbe5340c371570de77
Sergio Pascual <sergiopr@fis.ucm.es>
2015-10-07 18:28:37 +0200

    Add a function to run recipes using CLI

---
+++ emirdrp/tests/runrecipe.py
@@ -0,0 +1,24 @@
+#
+# Copyright 2015 Universidad Complutense de Madrid
+#
+# This file is part of PyEmir
+#
+# PyEmir is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyEmir is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyEmir.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from numina.user.cli import main
+
+
+def run_recipe():
+    main(['run', 'obsrun.yaml', '-r', 'control.yaml'])
