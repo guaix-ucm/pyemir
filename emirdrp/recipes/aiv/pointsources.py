7a7f7e294d65824ae2f4d9f17e72cc57f20410a4
Sergio Pascual <sergiopr@fis.ucm.es>
2016-06-13 22:16:17 +0200

    Create an alternative TEST6 recipe

---
+++ emirdrp/recipes/aiv/pointsources.py
@@ -0,0 +1,142 @@
+#
+# Copyright 2013-2016 Universidad Complutense de Madrid
+#
+# This file is part of PyEmir
+#
+# PyEmir is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyEmir is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyEmir.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""Test recipe to find point sources"""
+
+from __future__ import division
+
+import logging
+
+import numpy
+from astropy.stats import sigma_clipped_stats
+import photutils
+from numina.core import RecipeError
+from numina.core import Requirement, Product, Parameter
+from numina.core.requirements import ObservationResultRequirement
+from numina.core.products import ArrayType
+
+
+from emirdrp.core import EmirRecipe
+from emirdrp.products import DataFrameType
+from emirdrp.products import CoordinateList2DType
+from emirdrp.requirements import MasterBiasRequirement
+from emirdrp.requirements import MasterDarkRequirement
+from emirdrp.requirements import MasterIntensityFlatFieldRequirement
+from emirdrp.requirements import MasterSkyRequirement
+from .flows import basic_processing_with_combination
+from .flows import init_filters_bdfs
+from .common import pinhole_char, pinhole_char2
+from .common import get_dtur_from_header
+
+_logger = logging.getLogger('numina.recipes.emir')
+
+
+class TestPointSourceRecipe(EmirRecipe):
+
+    # Recipe Requirements
+    #
+    obresult = ObservationResultRequirement()
+    master_bias = MasterBiasRequirement()
+    master_dark = MasterDarkRequirement()
+    master_flat = MasterIntensityFlatFieldRequirement()
+    master_sky = MasterSkyRequirement()
+
+    pinhole_nominal_positions = Requirement(CoordinateList2DType,
+                                            'Nominal positions of the pinholes'
+                                            )
+    shift_coordinates = Parameter(True, 'Use header information to'
+                                  ' shift the pinhole positions from (0,0) '
+                                  'to X_DTU, Y_DTU')
+    box_half_size = Parameter(4, 'Half of the computation box size in pixels')
+    recenter = Parameter(True, 'Recenter the pinhole coordinates')
+    max_recenter_radius = Parameter(2.0, 'Maximum distance for recentering')
+
+    # Recipe Products
+    frame = Product(DataFrameType)
+    positions = Product(ArrayType)
+    positions_alt = Product(ArrayType)
+    DTU = Product(ArrayType)
+    filter = Product(str)
+    readmode = Product(str)
+    IPA = Product(float)
+    DETPA = Product(float)
+    DTUPA = Product(float)
+    param_recenter = Product(bool)
+    param_max_recenter_radius = Product(float)
+    param_box_half_size = Product(float)
+
+    def run(self, rinput):
+        _logger.info('starting processing for slit detection')
+
+        flow = init_filters_bdfs(rinput)
+
+        hdulist = basic_processing_with_combination(rinput, flow=flow)
+
+        hdr = hdulist[0].header
+        self.set_base_headers(hdr)
+
+        _logger.debug('finding point sources')
+
+        try:
+            filtername = hdr['FILTER']
+            readmode = hdr['READMODE']
+            ipa = hdr['IPA']
+            detpa = hdr['DETPA']
+            dtupa = hdr['DTUPA']
+            dtub, dtur = get_dtur_from_header(hdr)
+        except KeyError as error:
+            _logger.error(error)
+            raise RecipeError(error)
+
+        _logger.info('point source detection1')
+        data = hdulist[0].data
+
+        mean, median, std = sigma_clipped_stats(data, sigma=3.0, iters=5)
+
+        threshold = median + 5 * std
+        _logger.info('value of threshold for "daofind" %7.3f', threshold)
+
+        fwhm = 3.0
+        _logger.info('reference fwhm is %5.1f', fwhm)
+
+        tableresult = photutils.daofind(data, threshold=threshold, fwhm=fwhm)
+        ncolumns = len(tableresult.dtype)
+        positions = numpy.zeros((len(input), ncolumns), dtype=numpy.float64)
+        for n, (columnname, dummy) in enumerate(tableresult.dtype):
+            positions[:, n] = input[columnname].astype(numpy.float64)
+
+        _logger.info('point source detection2')
+        positions_alt = []
+
+
+        result = self.create_result(frame=hdulist,
+                                    positions=positions,
+                                    positions_alt=positions_alt,
+                                    filter=filtername,
+                                    DTU=dtub,
+                                    readmode=readmode,
+                                    IPA=ipa,
+                                    DETPA=detpa,
+                                    DTUPA=dtupa,
+                                    param_recenter=rinput.recenter,
+                                    param_max_recenter_radius=rinput.max_recenter_radius,
+                                    param_box_half_size=rinput.box_half_size
+                                    )
+        return result
+2
