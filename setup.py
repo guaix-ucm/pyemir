fdb1f8abe1e5f29c819bd553c382ed66e0725adc
Sergio Pascual <sergiopr@fis.ucm.es>
2016-06-14 18:13:52 +0200

    Increase the version of photutils in setup.py to >= 0.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name='pyemir',
       test_suite="emirdrp.tests",
       install_requires=['setuptools', 'numpy', 'scipy',
                         'numina>=0.14', 'astropy>=0.4',
-                        'matplotlib', 'six', 'photutils>=0.1',
+                        'matplotlib', 'six', 'photutils>=0.2',
                         'scikit-image>=0.11'],
       zip_safe=False,
       entry_points = {
