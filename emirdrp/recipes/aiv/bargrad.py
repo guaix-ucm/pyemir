5c342915dfe77a890bd95faf137c8e16c7dbaf49
Sergio Pascual <sergiopr@fis.ucm.es>
2016-09-17 18:39:53 +0200

    Move distortions to its own module

--- emirdrp/recipes/aiv/bargrad.py
+++ emirdrp/recipes/aiv/bargrad.py
@@ -265,43 +265,6 @@ class BarDetectionRecipe(EmirRecipe):
         return result
 
 
-def exvp(pos_x, pos_y):
-    # convert virtual pixel to real pixel
-    # convert world coordinate to pixel
-    center = [1024.5, 1024.5]
-    cf  = (0.1944 * 1 / 3600.0) * numpy. pi / 180.0
-    # ra = numpy.sqrt(($1-1024.5)**2+($2-1024.5)**2)
-    pos_base_x = pos_x - center[0]
-    pos_base_y = pos_y - center[1]
-    ra = numpy.hypot(pos_base_x, pos_base_y)
-    thet = numpy.arctan2(pos_base_y, pos_base_x)
-
-    r  = cf * ra
-    rr1 = 1+ 14606.7*r**2 +1739716115.1*r**4
-
-    nx1 = rr1 * ra * numpy.sin(thet) + center[0]
-    ny1 = rr1 * ra * numpy.cos(thet) + center[1]
-    return nx1, ny1
-
-
-def pvex(pos_x, pos_y):
-    # convert real pixel to virtual pixel.
-    # convert pixel to world coordinate
-    center = [1024.5, 1024.5]
-    cf  = (0.1944 * 1 / 3600.0) * numpy. pi / 180.0
-    pos_base_x = pos_x - center[0]
-    pos_base_y = pos_y - center[1]
-    ra = numpy.hypot(pos_base_x, pos_base_y)
-    thet = numpy.arctan2(pos_base_y, pos_base_x)
-
-    r  = cf * ra
-    rr1 = 1.000051-14892*r**2 -696254464*r**4
-
-    nx1 = rr1 * ra * numpy.sin(thet) + center[0]
-    ny1 = rr1 * ra * numpy.cos(thet) + center[1]
-    return nx1, ny1
-
-
 class BarDetectionDistRecipe(EmirRecipe):
 
     # Recipe Requirements
