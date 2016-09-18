a67c52d7fe13d6d4d641de5a549d731339c6e602
Sergio Pascual <sergiopr@fis.ucm.es>
2016-09-18 16:40:45 +0200

    Create BaseABBARecipe

---
+++ emirdrp/recipes/spec/subs.py
@@ -0,0 +1,104 @@
+#
+# Copyright 2016 Universidad Complutense de Madrid
+#
+# This file is part of PyEmir
+#
+# PyEmir is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyEmir is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyEmir.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+Spectroscopy mode, ABBA
+"""
+
+
+from numina.core import Product
+from numina.core.requirements import ObservationResultRequirement
+import numina.exceptions
+
+from emirdrp.core import EmirRecipe
+import emirdrp.products as prods
+import emirdrp.requirements as reqs
+import emirdrp.processing.datamodel
+from emirdrp.processing.combine import basic_processing
+import emirdrp.processing.info as info
+
+
+class BaseABBARecipe(EmirRecipe):
+    """Process images in ABBA mode"""
+
+    obresult = ObservationResultRequirement()
+    master_bpm = reqs.MasterBadPixelMaskRequirement()
+    master_bias = reqs.MasterBiasRequirement()
+    master_dark = reqs.MasterDarkRequirement()
+    master_flat = reqs.MasterSpectralFlatFieldRequirement()
+
+    spec_abba = Product(prods.DataFrameType)
+
+    def run(self, rinput):
+        self.logger.info('starting spectroscopy ABBA reduction')
+
+        flow = self.init_filters(rinput)
+        nimages = len(rinput.obresult.frames)
+        self.logger.info('we receive %d images', nimages)
+        if nimages != 4:
+            msg = 'Recipe expects 4 images, received %d' % nimages
+            raise numina.exceptions.RecipeError(msg)
+
+        procesed_hdulists = basic_processing(rinput, flow)
+
+        # INPUTS are ABBA, so
+        #
+        hdulist = self.process_abba(procesed_hdulists)
+        result = self.create_result(spec_abba=hdulist)
+        self.logger.info('end spectroscopy ABBA reduction')
+        return result
+
+    def process_abba(self, images):
+        # Process for images in ABBA mode
+        dataA0 = images[0][0].data
+        dataB0 = images[1][0].data
+
+        dataB1 = images[2][0].data
+        dataA1 = images[3][0].data
+
+        dataAB0 = dataA0 - dataB0
+        dataAB1 = dataA1 - dataB1
+
+        dataABBA = dataAB0 + dataAB1
+
+        hdulist = self.create_proc_hdulist(images, dataABBA)
+        self.logger.debug('update result header')
+        hdu = hdulist[0]
+        hdu.header['history'] = "Processed ABBA"
+        hdu.header['NUM-NCOM'] = (2, 'Number of combined frames')
+        dm = emirdrp.processing.datamodel.EmirDataModel()
+        for img, key in zip(images, ['A', 'B', 'B', 'A']):
+            imgid = dm.get_imgid(img)
+            hdu.header['history'] = "Image '{}' is '{}'".format(imgid, key)
+
+        return hdulist
+
+    def create_proc_hdulist(self, cdata, data_array):
+        import astropy.io.fits as fits
+        import uuid
+        # Copy header of first image
+        base_header = cdata[0][0].header.copy()
+
+        hdu = fits.PrimaryHDU(data_array, header=base_header)
+        self.set_base_headers(hdu.header)
+        hdu.header['EMIRUUID'] = uuid.uuid1().hex
+        # Headers of last image
+        hdu.header['TSUTC2'] = cdata[-1][0].header['TSUTC2']
+        result = fits.HDUList([hdu])
+        return result
---
+++ emirdrp/recipes/spec/tests/test_subs.py
@@ -0,0 +1,87 @@
+
+import pytest
+import astropy.io.fits as fits
+import numpy
+
+import numina.core
+import numina.exceptions
+
+from ..subs import BaseABBARecipe
+
+def create_frame(val=0, pos=3, keys=None):
+    data = numpy.zeros((10, 10))
+    data[pos] += val
+    hdu = fits.PrimaryHDU(data)
+    if keys:
+        for k, v in keys.items():
+            hdu.header[k] = v
+
+    return fits.HDUList([hdu])
+
+
+def create_frame_dark():
+    data = numpy.zeros((10, 10))
+    hdu = fits.PrimaryHDU(data)
+    hdu.header['EMIRUUID'] = '11111111111111111111111111111111'
+    return fits.HDUList([hdu])
+
+
+def create_frame_flat():
+    data = numpy.ones((10, 10))
+    hdu = fits.PrimaryHDU(data)
+    hdu.header['EMIRUUID'] = '2222222222222222222222222222222'
+    return fits.HDUList([hdu])
+
+def test_subs():
+
+    expected_data = numpy.zeros((10, 10))
+    expected_data[5] = 4
+    expected_data[3] = -4
+    keys = {'TSUTC1': 1000000001.00034, 'TSUTC2': 1000000100.00034}
+    frames1 = numina.core.DataFrame(frame=create_frame(val=2, pos=5, keys=keys))
+    keys = {'TSUTC1': 1000000101.00034, 'TSUTC2': 1000000200.00034}
+    frames2 = numina.core.DataFrame(frame=create_frame(val=2, pos=3, keys=keys))
+    keys = {'TSUTC1': 1000000201.00034, 'TSUTC2': 1000000300.00034}
+    frames3 = numina.core.DataFrame(frame=create_frame(val=2, pos=3, keys=keys))
+    keys = {'TSUTC1': 1000000301.00034, 'TSUTC2': 1000000400.00034}
+    frames4 = numina.core.DataFrame(frame=create_frame(val=2, pos=5, keys=keys))
+
+    obsresult = numina.core.ObservationResult()
+    obsresult.frames = [frames1, frames2, frames3, frames4]
+
+    recipe = BaseABBARecipe()
+    rinput = BaseABBARecipe.RecipeInput(
+        obresult=obsresult,
+        master_dark=numina.core.DataFrame(frame=create_frame_dark()),
+        master_flat=numina.core.DataFrame(frame=create_frame_flat())
+    )
+    result = recipe.run(rinput)
+
+    spec_abba_hdul = result.spec_abba.frame
+    assert spec_abba_hdul[0].header['TSUTC1'] == 1000000001.00034
+    assert spec_abba_hdul[0].header['TSUTC2'] == 1000000400.00034
+    assert 'NUM-DK' in spec_abba_hdul[0].header
+    assert spec_abba_hdul[0].header['NUM-NCOM'] == 2
+    assert numpy.allclose(expected_data, result.spec_abba.frame[0].data)
+
+
+def test_subs_raise():
+
+    expected_data = numpy.zeros((10, 10))
+    expected_data[5] = 4
+    expected_data[3] = -4
+    keys = {'TSUTC1': 1000000001.00034, 'TSUTC2': 1000000100.00034}
+    frames1 = numina.core.DataFrame(frame=create_frame(val=2, pos=5, keys=keys))
+
+    obsresult = numina.core.ObservationResult()
+    obsresult.frames = [frames1]
+
+    recipe = BaseABBARecipe()
+    rinput = BaseABBARecipe.RecipeInput(
+        obresult=obsresult,
+        master_dark=numina.core.DataFrame(frame=create_frame_dark()),
+        master_flat=numina.core.DataFrame(frame=create_frame_flat())
+    )
+
+    with pytest.raises(numina.exceptions.RecipeError):
+        recipe.run(rinput)
