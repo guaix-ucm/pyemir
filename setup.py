e1e1455ecca2593574612f6ea81dd16781e71c45
Nicolás Cardiel <cardiel@ucm.es>
2017-11-14 04:18:59 +0100

    Include package 'lmfit' in the list of install_requires.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(name='pyemir',
       install_requires=['setuptools', 'numpy', 'scipy',
                         'numina>=0.14', 'astropy>=1.1',
                         'matplotlib', 'six', 'photutils>=0.2',
-                        'sep>0.5', 'scikit-image>=0.11'],
+                        'sep>0.5', 'scikit-image>=0.11', 'lmfit'],
       zip_safe=False,
       entry_points={
           'numina.pipeline.1': [
