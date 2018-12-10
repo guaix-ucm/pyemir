f0cbf2c74e6aca8b9874b5c8e77578f17760e838
Sergio Pascual <sergiopr@fis.ucm.es>
2018-12-10 14:06:01 +0100

    [DOCS] Give correct copyright attribution to the UCM [skip ci]

--- doc/conf.py
+++ doc/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PyEmir'
-copyright = u'2010-2018, Sergio Pascual, Nicolás Cardiel'
+copyright = u'2010-2018, Universidad Complutense de Madrid'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
