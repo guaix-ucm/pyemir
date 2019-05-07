91664c147436b55b12d87f4795686b9dc9d2248c
Sergio Pascual <sergiopr@fis.ucm.es>
2019-05-07 18:28:29 +0200

    Move datamanager_remote fixture to conftest.py

--- emirdrp/conftest.py
+++ emirdrp/conftest.py
@@ -1,3 +1,55 @@
 
 from numina.tests.plugins import *
 
+import os
+import tarfile
+
+import pytest
+
+from numina.tests.testcache import download_cache
+from numina.user.helpers import create_datamanager
+
+
+@pytest.fixture(scope='module')
+def datamanager_remote(tmp_path_factory, request):
+    """Return a DataManager object create from a remote dataset"""
+    from numina.util.context import working_directory
+
+    req_base_default = "https://guaix.fis.ucm.es/data/"
+    req_base = getattr(request.module, 'TEST_SET_HOST', req_base_default)
+    req_tarname = getattr(request.module, 'TEST_SET_FILE')
+    req_datadir = getattr(request.module, 'TEST_SET_DATADIR', 'data')
+    req_control = getattr(request.module, 'TEST_SET_CONTROL', "control_v2.yaml")
+
+    basedir = tmp_path_factory.mktemp('manager')
+
+    datadir = basedir / req_datadir  # pathlib syntax
+    reqfile  = basedir / req_control
+
+    if req_tarname is None:
+        raise ValueError('Undefined TEST_SET_FILE')
+
+    url = req_base + req_tarname
+
+    # Download everything
+    with working_directory(basedir):
+
+        downloaded = download_cache(url)
+
+        # Uncompress
+        with tarfile.open(downloaded.name, mode="r:gz") as tar:
+            tar.extractall()
+
+        os.remove(downloaded.name)
+
+    # Insert OBS in the control file....
+    dm = create_datamanager(reqfile, basedir, datadir)
+
+    # This is not really needed...
+    # If everything is in the file already
+    # with working_directory(basedir):
+    #     obsresults = ['obs_ids.yaml']
+    #     sessions, loaded_obs = load_observations(obsresults, is_session=False)
+    #    dm.backend.add_obs(loaded_obs)
+
+    return dm
--- emirdrp/tests/test_img_tutorial.py
+++ emirdrp/tests/test_img_tutorial.py
@@ -1,77 +1,38 @@
 
-import os
-import tarfile
-
 import pytest
 
-from numina.tests.testcache import download_cache
 from numina.user.baserun import run_job
-from numina.user.helpers import create_datamanager
 
-
-@pytest.fixture(scope='module')
-def datamanagerfl(tmp_path_factory):
-    from numina.util.context import working_directory
-
-    basedir = tmp_path_factory.mktemp('manager')
-    datadir = basedir / 'data'  # pathlib syntax
-    reqfile  = basedir / 'control_v2.yaml'
-
-    tarname = "pyemir/pyemir-image-tutorial-v2.tar.gz"
-    base = "https://guaix.fis.ucm.es/data/"
-    url = base + tarname
-
-    # Download everything
-    with working_directory(basedir):
-
-        downloaded = download_cache(url)
-
-        # Uncompress
-        with tarfile.open(downloaded.name, mode="r:gz") as tar:
-            tar.extractall()
-
-        os.remove(downloaded.name)
-
-    # Insert OBS in the control file....
-    dm = create_datamanager(reqfile, basedir, datadir)
-
-    # This is not really needed...
-    # If everything is in the file already
-    # with working_directory(basedir):
-    #     obsresults = ['obs_ids.yaml']
-    #     sessions, loaded_obs = load_observations(obsresults, is_session=False)
-    #    dm.backend.add_obs(loaded_obs)
-
-    return dm
+# Variable for datamanager_remote
+TEST_SET_FILE = "pyemir/pyemir-image-tutorial-v2.tar.gz"
 
 
 @pytest.mark.remote_data
 @pytest.mark.parametrize("obsid", range(111, 118))
-def test_block1(datamanagerfl, obsid):
-    run_job(datamanagerfl, obsid)
+def test_block1(datamanager_remote, obsid):
+    run_job(datamanager_remote, obsid)
     assert True
 
 
 @pytest.mark.remote_data
 @pytest.mark.parametrize("obsid", range(121, 128))
-def test_block2(datamanagerfl, obsid):
-    print(datamanagerfl)
-    run_job(datamanagerfl, obsid)
+def test_block2(datamanager_remote, obsid):
+    run_job(datamanager_remote, obsid)
     assert True
 
 
 @pytest.mark.remote_data
 @pytest.mark.parametrize("obsid", [10101,20101])
-def test_block3(datamanagerfl, obsid):
-    run_job(datamanagerfl, obsid)
+def test_block3(datamanager_remote, obsid):
+    run_job(datamanager_remote, obsid)
     assert True
 
 
 @pytest.mark.remote_data
-def test_block4(datamanagerfl):
+def test_block4(datamanager_remote):
     from numina.util.context import working_directory
 
-    with working_directory(datamanagerfl.basedir):
+    with working_directory(datamanager_remote.basedir):
         with open('control_dump.yaml', 'w') as fp:
-            datamanagerfl.backend.dump(fp)
+            datamanager_remote.backend.dump(fp)
         assert True
