cd1be22da738a10cc9e5fd21573b15ed02a18c33
Sergio Pascual <sergiopr@fis.ucm.es>
2019-05-09 13:03:41 +0200

    Disable old remote tests

--- emirdrp/recipes/aiv/tests/test_mask.py
+++ emirdrp/recipes/aiv/tests/test_mask.py
@@ -20,18 +20,15 @@ BASE_URL = 'https://guaix.fis.ucm.es/data/pyemir/test/'
 
 
 @pytest.mark.remote_data
-def test_mode_TEST6_set0(numinatpldir):
-
+def _test_mode_TEST6_set0(numinatpldir):
     run_recipe()
 
 
 @pytest.mark.remote_data
-def test_mode_TEST7_set0(numinatpldir):
-
+def _test_mode_TEST7_set0(numinatpldir):
     run_recipe()
 
 
 @pytest.mark.remote_data
-def test_mode_TEST8_set0(numinatpldir):
-
+def _test_mode_TEST8_set0(numinatpldir):
     run_recipe()
