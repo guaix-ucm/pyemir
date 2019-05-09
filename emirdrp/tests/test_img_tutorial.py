84092106543efe6512536af84477a091b98053e9
Sergio Pascual <sergiopr@fis.ucm.es>
2019-05-09 13:03:09 +0200

    Add marker result_compare for tests in image tutorial

--- emirdrp/tests/test_img_tutorial.py
+++ emirdrp/tests/test_img_tutorial.py
@@ -1,31 +1,34 @@
 
 import pytest
 
-from numina.user.baserun import run_job
+from numina.user.baserun import run_reduce
 
 # Variable for datamanager_remote
 TEST_SET_FILE = "pyemir/pyemir-image-tutorial-v2.tar.gz"
 
 
+@pytest.mark.result_compare
 @pytest.mark.remote_data
 @pytest.mark.parametrize("obsid", range(111, 118))
 def test_block1(datamanager_remote, obsid):
-    run_job(datamanager_remote, obsid)
-    assert True
+    task = run_reduce(datamanager_remote, obsid)
+    return task.result
 
 
+@pytest.mark.result_compare
 @pytest.mark.remote_data
 @pytest.mark.parametrize("obsid", range(121, 128))
 def test_block2(datamanager_remote, obsid):
-    run_job(datamanager_remote, obsid)
-    assert True
+    task = run_reduce(datamanager_remote, obsid)
+    return task.result
 
 
+@pytest.mark.result_compare
 @pytest.mark.remote_data
-@pytest.mark.parametrize("obsid", [10101,20101])
+@pytest.mark.parametrize("obsid", [20101])
 def test_block3(datamanager_remote, obsid):
-    run_job(datamanager_remote, obsid)
-    assert True
+    task = run_reduce(datamanager_remote, obsid)
+    return task.result
 
 
 @pytest.mark.remote_data
