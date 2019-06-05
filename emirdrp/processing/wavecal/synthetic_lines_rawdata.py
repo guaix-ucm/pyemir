bdeb09cd6d6170694afd266ce0a26aa5cbeaeac1
Nicolás Cardiel <cardiel@ucm.es>
2019-06-05 19:29:07 +0200

    Connect pseudo-longslits in synthetic image

--- emirdrp/processing/wavecal/synthetic_lines_rawdata.py
+++ emirdrp/processing/wavecal/synthetic_lines_rawdata.py
@@ -35,7 +35,9 @@ from emirdrp.core import EMIR_NBARS
 def synthetic_lines_rawdata(catlines_all_wave,
                             catlines_all_flux,
                             list_useful_slitlets,
-                            rectwv_coeff):
+                            rectwv_coeff,
+                            ycorrection_pix=2.0,
+                            sigma_gauss_pix = 1.5):
     """Generate synthetic MOS data with arc/OH lines.
 
     Parameters
@@ -50,6 +52,11 @@ def synthetic_lines_rawdata(catlines_all_wave,
     rectwv_coeff : RectWaveCoeff instance
         Rectification and wavelength calibration coefficients for the
         particular CSU configuration.
+    ycorrection_pix : float
+        Approximate number of pixels from frontier to boundary.
+    sigma_gauss_pix : float
+        Sigma of the Gaussians to be employed to reproduce the
+        emission lines.
 
     Returns
     -------
@@ -66,14 +73,38 @@ def synthetic_lines_rawdata(catlines_all_wave,
     global_integer_offset_x_pix = rectwv_coeff.global_integer_offset_x_pix
     global_integer_offset_y_pix = rectwv_coeff.global_integer_offset_y_pix
 
+    nside_pix = int(sigma_gauss_pix * 5.0 + 0.5)
+
     cout = '0'
     for islitlet in range(1, EMIR_NBARS + 1):
         if islitlet in list_useful_slitlets:
             cout += '.'
             dumdict = rectwv_coeff.contents[islitlet - 1]
+            # check pseudo-longslit with previous slitlet
+            center = dumdict['csu_bar_slit_center']
+            width = dumdict['csu_bar_slit_width']
+            connected_prev = False
+            if islitlet > 1:
+                dumdict_prev = rectwv_coeff.contents[islitlet - 2]
+                center_prev = dumdict_prev['csu_bar_slit_center']
+                width_prev = dumdict_prev['csu_bar_slit_width']
+                if abs(center - center_prev) < width/2:
+                    if abs(width - width_prev) < width/2:
+                        connected_prev = True
+            # check pseudo-longslit with next slitlet
+            connected_next = False
+            if islitlet < EMIR_NBARS:
+                dumdict_next = rectwv_coeff.contents[islitlet]
+                center_next = dumdict_next['csu_bar_slit_center']
+                width_next = dumdict_next['csu_bar_slit_width']
+                if abs(center - center_next) < width/2:
+                    if abs(width - width_next) < width/2:
+                        connected_next = True
+            # wavelength calibration parameters
             crval1_linear = dumdict['crval1_linear']
             cdelt1_linear = dumdict['cdelt1_linear']
             crvaln_linear = crval1_linear + (EMIR_NAXIS1 - 1) * cdelt1_linear
+            # rectification parameters
             bb_ns1_orig = dumdict['bb_ns1_orig']
             ttd_order = dumdict['ttd_order']
             aij = dumdict['ttd_aij']
@@ -81,6 +112,7 @@ def synthetic_lines_rawdata(catlines_all_wave,
             min_row_rectified = float(dumdict['min_row_rectified'])
             max_row_rectified = float(dumdict['max_row_rectified'])
             wpoly_coeff = dumdict['wpoly_coeff']
+            # store coordinates for each line
             x1 = []
             y1 = []
             x2 = []
@@ -100,37 +132,54 @@ def synthetic_lines_rawdata(catlines_all_wave,
                                 x2.append(dum)
                                 y2.append(max_row_rectified)
                                 flist.append(flux)
-            pol_lower = Polynomial(dumdict['spectrail']['poly_coef_lower'])
-            pol_upper = Polynomial(dumdict['spectrail']['poly_coef_upper'])
             xx1, yy1 = fmap(ttd_order, aij, bij, np.array(x1), np.array(y1))
-            yy1 = pol_lower(xx1) - global_integer_offset_y_pix + 1
             xx1 -= global_integer_offset_x_pix
+            yy1 += bb_ns1_orig - global_integer_offset_y_pix
+            if not connected_prev:
+                yy1 += ycorrection_pix
             xx2, yy2 = fmap(ttd_order, aij, bij, np.array(x2), np.array(y2))
-            yy2 = pol_upper(xx2) - global_integer_offset_y_pix - 1
             xx2 -= global_integer_offset_x_pix
+            yy2 += bb_ns1_orig - global_integer_offset_y_pix
+            if not connected_next:
+                yy2 -= ycorrection_pix
 
             for xx1_, xx2_, yy1_, yy2_, flux in zip(xx1, xx2, yy1, yy2, flist):
                 slope = (xx2_ - xx1_) / (yy2_ - yy1_)
                 iyy1_ = int(yy1_ + 0.5)
+                if yy1_ <= float(iyy1_):
+                    fracpix1 = (float(iyy1_) - yy1_)
+                    iyy1_ -= 1
+                else:
+                    fracpix1 = 1.0 - (yy1_ - float(iyy1_))
                 iyy2_ = int(yy2_ + 0.5)
+                if yy2_ <= float(iyy2_):
+                    fracpix2 = 1.0 - (float(iyy2_) - yy2_)
+                else:
+                    fracpix2 = (yy2_ - float(iyy2_))
+                    iyy2_ += 1
                 for iyy_ in range(iyy1_, iyy2_ + 1):
+                    if iyy_ == iyy1_:
+                        fracpix = fracpix1
+                    elif iyy_ == iyy2_:
+                        fracpix = fracpix2
+                    else:
+                        fracpix = 1.0
                     icenter = iyy_ - 1
                     if 0 <= icenter <= EMIR_NAXIS2 - 1:
                         xx0_ = xx1_ + slope * (iyy_ - yy1_)
                         xcenter = int(xx0_ + 0.5)
-                        nside = 8
-                        npix = 2 * nside + 1
-                        xpix = xcenter + np.arange(-nside, nside + 1)
+                        npix = 2 * nside_pix + 1
+                        xpix = xcenter + np.arange(-nside_pix, nside_pix + 1)
                         left_border = xpix - xx0_ - 0.5
                         right_border = left_border + 1.0
                         borders = np.concatenate((left_border,
                                                   right_border))
-                        area = norm.cdf(borders, scale=1.5)
+                        area = norm.cdf(borders, scale=sigma_gauss_pix)
                         for i in range(npix):
                             xdum = xpix[i]
                             if 1 <= xdum <= EMIR_NAXIS1:
                                 simulated_image[icenter, xdum - 1] += \
-                                    flux * (area[i + npix] - area[i])
+                                    fracpix * flux * (area[i + npix] - area[i])
         else:
             cout += 'i'
 
