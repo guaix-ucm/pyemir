e9702440d2a6c407be535b6e9c12c8b4eff4e5bb
Nicolás Cardiel <cardiel@ucm.es>
2019-07-17 11:03:54 +0200

    Enable napoleon extension to read docstrings following the NumPy style

--- doc/conf.py
+++ doc/conf.py
@@ -33,7 +33,8 @@ import os
 sys.path.insert(0, os.path.abspath('../'))
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.imgmath',
-              'sphinx.ext.coverage', 'sphinx.ext.intersphinx']
+              'sphinx.ext.coverage', 'sphinx.ext.intersphinx',
+              'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
