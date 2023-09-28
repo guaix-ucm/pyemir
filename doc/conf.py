fdcb1459a6c6e381c1899b43465be9d9db3d5d96
Nicolás Cardiel <cardiel@ucm.es>
2023-09-28 18:06:24 +0200

    Make use of Read the Docs Sphinx theme for documentation

--- doc/conf.py
+++ doc/conf.py
@@ -36,7 +36,7 @@ sys.path.insert(0, os.path.abspath('../'))
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.imgmath',
               'sphinx.ext.coverage', 'sphinx.ext.intersphinx',
-              'sphinx.ext.napoleon']
+              'sphinx.ext.napoleon', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -104,7 +104,8 @@ modindex_common_prefix = ['emirdrp.', ]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+# html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
