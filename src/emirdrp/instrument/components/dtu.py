6a7b100ad07f1bb194a9b748cfbcdc8ff7bfa94e
Sergio Pascual <sergiopr@fis.ucm.es>
2023-11-23 16:16:54 +0100

    Add dtu component file

---
+++ src/emirdrp/instrument/components/dtu.py
@@ -0,0 +1,14 @@
+#
+# Copyright 2019-2023 Universidad Complutense de Madrid
+#
+# This file is part of EMIR DRP
+#
+# SPDX-License-Identifier: GPL-3.0+
+# License-Filename: LICENSE.txt
+#
+
+from numina.instrument.hwdevice import HWDevice
+
+
+class DetectorTranslationUnit(HWDevice):
+    pass
