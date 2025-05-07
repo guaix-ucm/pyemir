77945155015b7ae122397958accd3c9b8426b035
Sergio Pascual <sergiopr@fis.ucm.es>
2025-05-07 18:26:24 +0200

    Remove methods moved to BaseRecipe

--- src/emirdrp/core/recipe.py
+++ src/emirdrp/core/recipe.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2008-2023 Universidad Complutense de Madrid
+# Copyright 2008-2025 Universidad Complutense de Madrid
 #
 # This file is part of PyEmir
 #
@@ -8,11 +8,8 @@
 #
 
 import logging
-import collections.abc
 
-import numina.ext.gtc
 import numina.core.recipes as recipes
-import numina.util.flow as flowmod
 
 import emirdrp.core.correctors as cor
 import emirdrp.processing.info
@@ -38,59 +35,11 @@ class EmirRecipe(recipes.BaseRecipe):
 
         return imgtypes, getters
 
-    def get_filters(self, imgtypes, getters):
-
-        used_getters = []
-        for rtype, getter in zip(imgtypes, getters):
-            self.logger.debug('get_filters, %s  %s', rtype, getter)
-            if rtype is None:
-                # Unconditional
-                if isinstance(getter, collections.abc.Iterable):
-                    used_getters.extend(getter)
-                else:
-                    used_getters.append(getter)
-            else:
-                # Search
-                for key, val in self.RecipeInput.stored().items():
-                    if isinstance(val.type, rtype):
-                        if isinstance(getter, collections.abc.Iterable):
-                            used_getters.extend(getter)
-                        else:
-                            used_getters.append(getter)
-                        break
-                else:
-                    pass
-        return used_getters
-
-    def init_filters_generic(self, rinput, getters_seq, ins):
-        # with BPM, bias, dark, flat and sky
-        if numina.ext.gtc.check_gtc():
-            self.logger.debug('running in GTC environment')
-        else:
-            self.logger.debug('running outside of GTC environment')
-
-        meta = self.gather_info(rinput)
-        self.logger.debug('obresult info')
-        for entry in meta['obresult']:
-            self.logger.debug('frame info is %s', entry)
-
-        reduction_flows = []
-        for getters in getters_seq:
-            correctors = [getter(rinput, meta, ins, self.datamodel)
-                          for getter in getters]
-            flow = flowmod.SerialFlow(correctors)
-            reduction_flows.append(flow)
-
-        return reduction_flows
-
-    def init_filters(self, rinput, ins=None):
-        if ins is None:
-            ins = rinput.obresult.configuration
+    def create_getters(self):
         imgtypes, getters = self.types_getter()
         getters = self.get_filters(imgtypes, getters)
         getters_seq = [getters]
-        # FIXME:
-        return self.init_filters_generic(rinput, getters_seq, ins)[0]
+        return getters_seq
 
     def aggregate_result(self, result, rinput):
         return result
